02887b20fc66f7763f29d615128077222f9d70fb
Mike Conway <mike.conway@nih.gov>
2021-01-21 07:50:37 -0500

    #2 start on a connection factory

--- pkg/core/irods_connection.go
+++ pkg/core/irods_connection.go
@@ -1,13 +1,19 @@
 // package represents the base connection to an iRODS agent
 package irods_connection
 
-import "net"
+import {
+	"net"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"irods_context"
+}
 
 // a connection to an iRODS agent
 type IrodsConnection stuct {
 	IrodsAccount IrodsAccount
 	AuthResult AuthResult
 	IPConn IrodsConnection 
+	ConnStatus ConnStatus = CONN_INIT
 }
 
 // represents the result of an authentication
@@ -19,10 +25,20 @@ type AuthResult struct {
 
 // auth type for iRODS login
 type AuthType string
+// connection status
+type ConnStatus string
 
 const (
     STANDARD   AuthType = "Standard"
     PAM AuthType = "PAM"
+)
+
+// enumerates status of an agent connection
+const (
+	CONN_INIT   ConnStatus = "init"
+	CONN_PRE_AUTH   ConnStatus = "pre-auth"
+	CONN_CONNECTED  ConnStatus = "connected"
+	CONN_CLOSED   ConnStatus = "closed"
 )
 
 type IrodsAccount struct {
@@ -34,4 +50,8 @@ type IrodsAccount struct {
 	AdminUserNameForProxy string 
 
 }
+
+func ConnectionFactory(irodsAccount IrodsAccount, irodsContext IrodsContext)
+
+
 
